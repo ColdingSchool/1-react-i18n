f8de097dde053310258a703ae3cef05f64c22bff
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2023-02-08 20:22:30 -0300

    refactor the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { Typography, Stack, Divider } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 
-import { LanguageSelect } from './components/LanguageSelect'
 import brazilIconPng from './icons/brazil.png'
 import euaIconPng from './icons/eua.png'
 
 export function App() {
   const { i18n, t } = useTranslation()
-  const { language } = i18n
+  const { language, changeLanguage } = i18n
 
   return (
-    <Stack>
-      <LanguageSelect />
+    <div>
+      <form>
+        <label htmlFor="language-select">Pick a Language</label>
+        <select name="language-select" value={language} onChange={e => changeLanguage(e.target.value)}>
+          <option value="pt-BR">Português</option>
+          <option value="en-US">Inglês</option>
+        </select>
+      </form>
 
-      <Divider sx={{ my: 5 }}>
-        <img src={language === 'pt-BR' ? brazilIconPng : euaIconPng} />
-      </Divider>
+      <h1>{t('title')}</h1>
+      <h2>{t('home.description')}</h2>
 
-      <Typography variant="h3">{t('title')}</Typography>
-      <Typography variant="subtitle1">{t('home.description')}</Typography>
-    </Stack>
+      <img width={64} height={64} src={language === 'pt-BR' ? brazilIconPng : euaIconPng} />
+    </div>
   )
 }
